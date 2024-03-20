8e2514960f0f711dea39082cb044dbf3472f4b99
yiqiao <yimingqiao3163@gmail.com>
2024-03-20 15:40:05 +0800

    ready to next

--- simd_micro_bench.cpp
+++ simd_micro_bench.cpp
@@ -67,47 +67,42 @@ int main(int argc, char *argv[]) {
   std::vector<uint32_t> sel_vector(kBlockSize);
   for (uint32_t i = 0; i < kBlockSize; ++i) { sel_vector[i] = i; }
 
-  // LHS: 64-bit keys. RHS: 64-bit keys, and 64-bit payloads
-  std::cout << "Work set size (LHS Block Size + RHS Hash Table Size): " << 8 * (kBlockSize) / 1024.0 << " KB + "
-            << 8 * (kRHSTuples * 2) / 1024.0 << " KB\n";
+  std::cout << "--------------- SIMD Chaining HashJoin ---------------\n";
+  {
+    CycleProfiler::Get().Init();
+    HashTable hash_table(kRHSTuples, kChunkFactor);
+    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
+    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
+    Vector keys_block(AttributeType::INTEGER);
+    uint64_t n_tuples = 0;
 
-  // this code is to be tested.
-  //  std::cout << "--------------- SIMD Chaining HashJoin ---------------\n";
-  //  {
-  //    CycleProfiler::Get().Init();
-  //    HashTable hash_table(kRHSTuples, kChunkFactor);
-  //    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
-  //    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
-  //    Vector keys_block(AttributeType::INTEGER);
-  //    uint64_t n_tuples = 0;
-  //
-  //    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
-  //      // load one block
-  //      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
-  //      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
-  //      keys_block.count_ = n_filling;
-  //      input.data_[0] = keys_block;
-  //      input.count_ = n_filling;
-  //
-  //      for (uint32_t i = 0; i < 1; i++) {
-  //        // Function Probe.
-  //        auto scan_structure = hash_table.SIMDProbe(keys_block);
-  //
-  //        // Function Next.
-  //        while (scan_structure.HasNext()) { n_tuples += scan_structure.SIMDNext(keys_block, input, output); }
-  //      }
-  //    }
-  //
-  //    std::vector<double> cpts(4);
-  //    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
-  //    CycleProfiler::Get().Init();
-  //    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
-  //    std::cout << "Match Tuples: " << cpts[1] << "\t";
-  //    std::cout << "Gather Tuples: " << cpts[2] << "\t";
-  //    std::cout << "Advance Pointers: " << cpts[3] << "\n";
-  //    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
-  //    std::cout << "#tuples: " << n_tuples << "\n";
-  //  }
+    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
+      // load one block
+      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
+      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
+      keys_block.count_ = n_filling;
+      input.data_[0] = keys_block;
+      input.count_ = n_filling;
+
+      for (uint32_t i = 0; i < 1; i++) {
+        // Function Probe.
+        auto scan_structure = hash_table.SIMDProbe(keys_block);
+
+        // Function Next.
+        while (scan_structure.HasNext()) { n_tuples += scan_structure.SIMDNext(keys_block, input, output); }
+      }
+    }
+
+    std::vector<double> cpts(4);
+    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
+    CycleProfiler::Get().Init();
+    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
+    std::cout << "Match Tuples: " << cpts[1] << "\t";
+    std::cout << "Gather Tuples: " << cpts[2] << "\t";
+    std::cout << "Advance Pointers: " << cpts[3] << "\n";
+    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
+    std::cout << "#tuples: " << n_tuples << "\n";
+  }
 
   std::cout << "--------------- Scalar Chaining HashJoin ---------------\n";
   {
@@ -181,77 +176,77 @@ int main(int argc, char *argv[]) {
     std::cout << "#tuples: " << n_tuples << "\n";
   }
 
-  //  std::cout << "--------------- Scalar (In One) Chaining HashJoin ---------------\n";
-  //  {
-  //    CycleProfiler::Get().Init();
-  //    HashTable hash_table(kRHSTuples, kChunkFactor);
-  //    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
-  //    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
-  //    Vector keys_block(AttributeType::INTEGER);
-  //    uint64_t n_tuples = 0;
-  //
-  //    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
-  //      // load one block
-  //      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
-  //      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
-  //      keys_block.count_ = n_filling;
-  //      input.data_[0] = keys_block;
-  //      input.count_ = n_filling;
-  //
-  //      for (uint32_t i = 0; i < 1; i++) {
-  //        // Function Probe.
-  //        auto scan_structure = hash_table.Probe(keys_block);
-  //
-  //        // Function Next.
-  //        while (scan_structure.HasNext()) { n_tuples += scan_structure.InOneNext(keys_block, input, output); }
-  //      }
-  //    }
-  //
-  //    std::vector<double> cpts(4);
-  //    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
-  //    CycleProfiler::Get().Init();
-  //    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
-  //    std::cout << "Match & Gather & Advance: " << cpts[1] << "\n";
-  //    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
-  //    std::cout << "#tuples: " << n_tuples << "\n";
-  //  }
-  //
-  //  std::cout << "--------------- SIMD Linear Probing HashJoin ---------------\n";
-  //  {
-  //    CycleProfiler::Get().Init();
-  //    LPHashTable hash_table(kRHSTuples, kChunkFactor);
-  //    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
-  //    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
-  //    Vector keys_block(AttributeType::INTEGER);
-  //
-  //    uint64_t n_tuples = 0;
-  //
-  //    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
-  //      // load one block
-  //      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
-  //      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
-  //      keys_block.count_ = n_filling;
-  //      input.data_[0] = keys_block;
-  //      input.count_ = n_filling;
-  //
-  //      for (uint32_t i = 0; i < 1; i++) {
-  //        // Function Probe.
-  //        auto scan_structure = hash_table.SIMDProbe(keys_block);
-  //
-  //        // Function Next.
-  //        while (scan_structure.HasNext()) { n_tuples += scan_structure.SIMDNext(keys_block, input, output); }
-  //      }
-  //    }
-  //
-  //    std::vector<double> cpts(4);
-  //    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
-  //    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
-  //    std::cout << "Match Tuples: " << cpts[1] << "\t";
-  //    std::cout << "Gather Tuples: " << cpts[2] << "\t";
-  //    std::cout << "Advance Pointers: " << cpts[3] << "\n";
-  //    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
-  //    std::cout << "#tuples: " << n_tuples << "\n";
-  //  }
+  std::cout << "--------------- Scalar (In One) Chaining HashJoin ---------------\n";
+  {
+    CycleProfiler::Get().Init();
+    HashTable hash_table(kRHSTuples, kChunkFactor);
+    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
+    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
+    Vector keys_block(AttributeType::INTEGER);
+    uint64_t n_tuples = 0;
+
+    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
+      // load one block
+      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
+      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
+      keys_block.count_ = n_filling;
+      input.data_[0] = keys_block;
+      input.count_ = n_filling;
+
+      for (uint32_t i = 0; i < 1; i++) {
+        // Function Probe.
+        auto scan_structure = hash_table.Probe(keys_block);
+
+        // Function Next.
+        while (scan_structure.HasNext()) { n_tuples += scan_structure.InOneNext(keys_block, input, output); }
+      }
+    }
+
+    std::vector<double> cpts(4);
+    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
+    CycleProfiler::Get().Init();
+    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
+    std::cout << "Match & Gather & Advance: " << cpts[1] << "\n";
+    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
+    std::cout << "#tuples: " << n_tuples << "\n";
+  }
+
+  std::cout << "--------------- SIMD Linear Probing HashJoin ---------------\n";
+  {
+    CycleProfiler::Get().Init();
+    LPHashTable hash_table(kRHSTuples, kChunkFactor);
+    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
+    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
+    Vector keys_block(AttributeType::INTEGER);
+
+    uint64_t n_tuples = 0;
+
+    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
+      // load one block
+      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
+      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
+      keys_block.count_ = n_filling;
+      input.data_[0] = keys_block;
+      input.count_ = n_filling;
+
+      for (uint32_t i = 0; i < 1; i++) {
+        // Function Probe.
+        auto scan_structure = hash_table.SIMDProbe(keys_block);
+
+        // Function Next.
+        while (scan_structure.HasNext()) { n_tuples += scan_structure.SIMDNext(keys_block, input, output); }
+      }
+    }
+
+    std::vector<double> cpts(4);
+    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
+    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
+    std::cout << "Match Tuples: " << cpts[1] << "\t";
+    std::cout << "Gather Tuples: " << cpts[2] << "\t";
+    std::cout << "Advance Pointers: " << cpts[3] << "\n";
+    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
+    std::cout << "#tuples: " << n_tuples << "\n";
+  }
 
   std::cout << "--------------- Scalar Linear Probing HashJoin ---------------\n";
   {
@@ -325,38 +320,38 @@ int main(int argc, char *argv[]) {
     std::cout << "#tuples: " << n_tuples << "\n";
   }
 
-  //  std::cout << "--------------- Scalar (In One) Linear Probing HashJoin ---------------\n";
-  //  {
-  //    CycleProfiler::Get().Init();
-  //    LPHashTable hash_table(kRHSTuples, kChunkFactor);
-  //    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
-  //    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
-  //    Vector keys_block(AttributeType::INTEGER);
-  //
-  //    uint64_t n_tuples = 0;
-  //
-  //    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
-  //      // load one block
-  //      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
-  //      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
-  //      keys_block.count_ = n_filling;
-  //      input.data_[0] = keys_block;
-  //      input.count_ = n_filling;
-  //
-  //      for (uint32_t i = 0; i < 1; i++) {
-  //        // Function Probe.
-  //        auto scan_structure = hash_table.Probe(keys_block);
-  //
-  //        // Function Next.
-  //        while (scan_structure.HasNext()) { n_tuples += scan_structure.InOneNext(keys_block, input, output); }
-  //      }
-  //    }
-  //
-  //    std::vector<double> cpts(4);
-  //    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
-  //    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
-  //    std::cout << "Match & Gather & Advance: " << cpts[1] << "\n";
-  //    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
-  //    std::cout << "#tuples: " << n_tuples << "\n";
-  //  }
+  std::cout << "--------------- Scalar (In One) Linear Probing HashJoin ---------------\n";
+  {
+    CycleProfiler::Get().Init();
+    LPHashTable hash_table(kRHSTuples, kChunkFactor);
+    DataChunk input(vector<AttributeType>{AttributeType::INTEGER});
+    DataChunk output(vector<AttributeType>{AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER});
+    Vector keys_block(AttributeType::INTEGER);
+
+    uint64_t n_tuples = 0;
+
+    for (uint32_t k = 0; k < kLHSTuples; k += kBlockSize) {
+      // load one block
+      size_t n_filling = std::min(kBlockSize, kLHSTuples - k);
+      for (uint32_t i = 0; i < n_filling; ++i) { keys_block.GetValue(i) = keys[k + i]; }
+      keys_block.count_ = n_filling;
+      input.data_[0] = keys_block;
+      input.count_ = n_filling;
+
+      for (uint32_t i = 0; i < 1; i++) {
+        // Function Probe.
+        auto scan_structure = hash_table.Probe(keys_block);
+
+        // Function Next.
+        while (scan_structure.HasNext()) { n_tuples += scan_structure.InOneNext(keys_block, input, output); }
+      }
+    }
+
+    std::vector<double> cpts(4);
+    for (size_t i = 0; i < 4; i++) { cpts[i] = double(CycleProfiler::Get().Data(i)) / double(kLHSTuples); }
+    std::cout << "Hash & Find Bucket: " << cpts[0] << "\t";
+    std::cout << "Match & Gather & Advance: " << cpts[1] << "\n";
+    std::cout << "Total: " << cpts[0] + cpts[1] + cpts[2] + cpts[3] << "\n";
+    std::cout << "#tuples: " << n_tuples << "\n";
+  }
 }
